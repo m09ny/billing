4e027f8436bd78a621974c50a290394fa776e361
Szilard Kedves <szilard.kedves@siemens.com>
2019-06-25 21:44:52 +0300

    latest

--- src/main/java/com/billing/BillingApplication.java
+++ src/main/java/com/billing/BillingApplication.java
@@ -2,6 +2,7 @@ package com.billing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class BillingApplication {
