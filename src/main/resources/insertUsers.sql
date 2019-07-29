ce5b98d3a8e5d1c0ab8d33bd36a5be315406ace5
Szilard Kedves <szilard.kedves@siemens.com>
2019-07-29 22:02:43 +0300

    updated users

--- src/main/resources/insertUsers.sql
+++ src/main/resources/insertUsers.sql
@@ -1,1 +1,4 @@
-INSERT INTO USERS(password, username, isAdmin, salt) VALUES('IDDJ/Ps+UMwCJ7U53DFuxw==', 'admin', true, 'ozqSrkVXHnjEaKVsNOXQwg==');
+INSERT INTO USERS(password, username, isAdmin, salt) VALUES('PYVitCsKogDEPeXtCuxJRQ==', 'admin', true, 'vyKNmkz2UBfVf/7cerTF6w==');
+INSERT INTO USERS(password, username, isAdmin, salt) VALUES('N0UTEE6DO3jYOm4rsRZVsA==', 'user1', false, 'Eqcsm7SNvXwL5X7zzjuHrQ==');
+INSERT INTO USERS(password, username, isAdmin, salt) VALUES('p1qXD8+IJ7yWs251pk09bw==', 'user2', false, 'I0SmOy8pIuvyN43XZYCbMA==');
+INSERT INTO USERS(password, username, isAdmin, salt) VALUES('6r8SOP2GOzqw+KiOIVEKNw==', 'user3', false, 'q/z0CPZYlxgrJx2HkiXWUg==');
